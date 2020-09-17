1464302f86bc67d9d8184cb41f8a265298cd1f84
Undeio <sean_link@live.com>
2020-09-18 07:50:34 +0800

    Site updated: 2020-09-18 07:50:34

--- js/utterances.js
+++ js/utterances.js
@@ -1,9 +1,9 @@
 /* utterances 评论插件 */
 // 初始化
-window.onload = function () {
+window.addEventListener("DOMContentLoaded", function () {
   query2add();
-};
-// pjax 使用 pushState() 不会触发 hashchange 事件，需要拦截一下
+});
+// pjax 使用 pushState() 不会触发 hashchange 事件，需要拦截
 var _wr = function (type) {
   var orig = history[type];
   return function () {
@@ -20,6 +20,7 @@ history.pushState = _wr("pushState");
 
 /* 监听 change 事件 */
 window.addEventListener("pushState", function (e) {
+  // e && e.arguments.length > 2 && e.arguments[2]
   query2add();
 });
 
@@ -33,14 +34,15 @@ function loadUtterance() {
   js.setAttribute("crossorigin", "anonymous");
   js.setAttribute("async", "async");
   document.getElementsByClassName("main-inner")[0].appendChild(js);
+  console.log(js);
 }
 
 /* 是否已存在组件 */
 function query2add() {
-  let postCopyRight = document.getElementsByClassName("post-copyright")[0];
-  if (postCopyRight) {
-    let utterances = document.getElementsByClassName("utterances")[0];
-    if (utterances) {
+  let postCopyRight = document.getElementsByClassName("post-copyright");
+  if (postCopyRight && postCopyRight[0]) {
+    let utterances = document.getElementsByClassName("utterances");
+    if (utterances && utterances[0]) {
       return;
     }
     loadUtterance();
