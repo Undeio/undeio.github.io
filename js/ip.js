fd17bf564d5c1e42d387ae45abe1897972e3a768
Undeio <sean_link@live.com>
2020-09-17 09:40:41 +0800

    Site updated: 2020-09-17 09:40:41

---
+++ js/ip.js
@@ -0,0 +1,14 @@
+printIP();
+function printIP() {
+  fetch("https://ipapi.co/json/").then((res) => {
+    if (res.ok) {
+      res.json().then((data) => {
+        const ip = data.ip;
+        let node = document.createElement("div");
+        node.innerHTML = "本次访问IP: " + ip;
+        document.getElementsByClassName("footer-inner")[0].appendChild(node);
+        return ip;
+      });
+    }
+  });
+}
