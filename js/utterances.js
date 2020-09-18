af19ce42fee364350a07ce33f26db147c264aaf8
Undeio <sean_link@live.com>
2020-09-18 12:31:43 +0800

    Site updated: 2020-09-18 12:31:43

--- js/utterances.js
+++ js/utterances.js
@@ -1,8 +1,10 @@
 /* utterances 评论插件 */
+
 // 初始化
 window.addEventListener("DOMContentLoaded", function () {
   query2add();
 });
+
 // pjax 使用 pushState() 不会触发 hashchange 事件，需要拦截
 var _wr = function (type) {
   var orig = history[type];
@@ -18,17 +20,17 @@ var _wr = function (type) {
 
 history.pushState = _wr("pushState");
 
-/* 监听 change 事件 */
+// 监听 change 事件
 window.addEventListener("pushState", function (e) {
   // e && e.arguments.length > 2 && e.arguments[2]
   query2add();
 });
 
-/* 加载 utterance 插件 */
+// 加载 utterance 插件
 function loadUtterance() {
   let js = document.createElement("script");
   js.setAttribute("src", "https://utteranc.es/client.js");
-  js.setAttribute("repo", "Undeio/comments-of-blog");
+  js.setAttribute("repo", "Undeio/undeio.github.io");
   js.setAttribute("issue-term", "title");
   js.setAttribute("theme", "github-light");
   js.setAttribute("crossorigin", "anonymous");
@@ -37,10 +39,12 @@ function loadUtterance() {
   console.log(js);
 }
 
-/* 是否已存在组件 */
+// 是否已存在组件
 function query2add() {
+  // 只有正文页面才有 post-copyright，也就是最后的版权声明
   let postCopyRight = document.getElementsByClassName("post-copyright");
   if (postCopyRight && postCopyRight[0]) {
+  // 存在评论就放弃添加
     let utterances = document.getElementsByClassName("utterances");
     if (utterances && utterances[0]) {
       return;
