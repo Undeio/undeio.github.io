0dae563fdef811b13cc2446932a07b809617825d
Undeio <sean_link@live.com>
2022-05-18 11:44:40 +0800

    Site updated: 2022-05-18 11:44:39

--- js/plugins-injects.js
+++ js/plugins-injects.js
@@ -1,4 +1,4 @@
-// busuanzi
+/* busuanzi */
 !(function () {
   const parent = document.querySelector(".busuanzi");
   const child = document.querySelector("#busuanzi_value_site_uv");
@@ -15,11 +15,11 @@
   }, 0);
 })();
 
-// ip
+/* ip */
 !(function () {
   const ip = document.querySelector(".access-ip");
   try {
-    ip.innerText = `当前访问IP: ${returnCitySN["cip"]} ${returnCitySN["cname"]}`;
+    ip.innerText = `访问IP: ${returnCitySN["cip"]} ${returnCitySN["cname"]}`;
     ip.style.display = "block";
   } catch (error) {
     console.error(
@@ -30,7 +30,7 @@
     fetch("https://ipapi.co/ip/")
       .then(function (response) {
         response.text().then((txt) => {
-          ip.innerText = `当前访问IP: ${txt}`;
+          ip.innerText = `访问IP: ${txt}`;
           ip.style.display = "block";
         });
       })
@@ -40,30 +40,14 @@
   }
 })();
 
-// runtime
+/* runtime */
 !(function () {
+  // 引入 luxon 处理时间
+  const DateTime = luxon.DateTime;
+  // 缓存 DOM 查询
+  const runtime = document.querySelector(".runtime");
+
   customizeSetInterval(addRunTime, 1000);
-
-  function addRunTime() {
-    // 建站时间
-    const since = "2020-05-01 12:00:00";
-
-    const dateNow = moment(new Date());
-    const dateSince = moment(since);
-    const dateDiff = moment.duration(dateNow.diff(dateSince));
-    const dateInterval = dateDiff._data;
-
-    // 前导 0
-    for (let key in dateInterval) {
-      dateInterval[key] =
-        dateInterval[key] < 10
-          ? "0" + dateInterval[key]
-          : "" + dateInterval[key];
-    }
-    // 添加
-    const runtime = document.querySelector(".runtime");
-    runtime.innerText = `本站已重建 ${dateInterval.years} 年 ${dateInterval.months} 个月 ${dateInterval.days} 天 ${dateInterval.hours} 小时 ${dateInterval.minutes} 分 ${dateInterval.seconds} 秒`;
-  }
 
   function customizeSetInterval(fn, interval) {
     let startTime = Date.now();
@@ -78,14 +62,46 @@
     }
     loop();
   }
+
+  function addRunTime() {
+    // 建站时间
+    const since = "2020-05-01T12:00:00";
+    // 固定时区，不受系统时区影响
+    const timeZone = "Asia/Shanghai";
+
+    const dateNow = DateTime.now().setZone(timeZone);
+    const dateSince = DateTime.fromISO(since).setZone(timeZone);
+    const dateInterval = dateNow
+      .diff(dateSince, [
+        "years",
+        "months",
+        "days",
+        "hours",
+        "minutes",
+        "seconds",
+      ])
+      .toObject();
+
+    runtime.innerText = `悠然前行${dateInterval.years}年${
+      dateInterval.months
+    }个月${dateInterval.days}天${dateInterval.hours}小时${
+      dateInterval.minutes
+    }分${
+      dateInterval.seconds.toFixed() < 10
+        ? "0" + dateInterval.seconds.toFixed()
+        : dateInterval.seconds.toFixed()
+    }秒`;
+  }
 })();
 
-// pjax - busuanzi
+/* pjax - 修正 busuanzi 统计 */
 !(function () {
-  document.addEventListener("pjax:success", function (e) {
-    const result = /\posts/g.test(e.target.URL);
-    if (result) {
-      const ibruce = document.querySelector("#busuanzi-script");
+  // 缓存 DOM 查询
+  const ibruce = document.querySelector("#busuanzi-script");
+
+  document.addEventListener("pjax:success", function (event) {
+    const flag = new RegExp("posts", "g").test(event.target.URL);
+    if (flag) {
       const busuanzi = document.createElement("script");
       busuanzi.setAttribute("src", ibruce.getAttribute("src"));
       busuanzi.setAttribute("async", true);
